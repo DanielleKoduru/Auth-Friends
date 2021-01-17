0b9ae5b24112198e300cf93043498c7c395a3d2d
Danielle Koduru <69174710+DanielleKoduru@users.noreply.github.com>
2021-01-17 16:49:26 -0700

    app.js and login.js done, no styling

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import PrivateRoute from "./Components/PrivateRoute";
+import Login from './Components/Login';
+import FriendList from './Components/FriendList';
+
 import './App.css';
 
-function App() {
+function App(props) {
+  const logout = () => {
+    localStorage.removeItem("token");
+    props.history.push("/");
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Friends List App</h1>
+          <Link to="/Login">Login</Link>
+          <Link onClick={logout}>Logout</Link>
+          <Link to="/protected">Friends</Link>
+        
+          <Switch>
+            <PrivateRoute exact path="/protected" component={FriendList} />
+            <Route path="/login" component={Login} />
+            <Route component={Login} />
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ friends/src/Components/Login.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import axios from 'axios';
+
+class Login extends React.Component {
+  state = {
+    credentials: {
+      username: '',
+      password: ''
+    }
+  };
+
+  handleChange = e => {
+    this.setState({
+      credentials: {
+        ...this.state.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  login = e => {
+    e.preventDefault();
+    // hit the login API endpoint with the username and password in the request body
+    axios.post("http://localhost:5000/api/login", this.state.credentials)
+      .then(res => {
+        // happy path: store the token from the response as a localStorage item called "token", redirect to logged-in route
+        localStorage.setItem("token", res.data.payload);
+        this.props.history.push("/protected");
+      })
+      .catch(err => console.log(err));
+    // sad path: just log it for now
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.login}>
+          <input
+            type="text"
+            name="username"
+            value={this.state.credentials.username}
+            onChange={this.handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={this.state.credentials.password}
+            onChange={this.handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default Login;
